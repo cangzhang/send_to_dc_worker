981472358cd1c3c8ae2c817460331ed7b96d7fa6
cangzhang <cangzan@gmail.com>
2025-07-16 12:11:00 +0800

    refactor: remove unused SVG assets and update content script match patterns

--- ext/entrypoints/popup/App.tsx
+++ ext/entrypoints/popup/App.tsx
@@ -1,34 +1,23 @@
 import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const getTabDetail = async () => {
+    const tab = await browser.tabs.query({ active: true, currentWindow: true });
+    if (tab?.[0]) {
+      const currentTab = tab[0];
+      console.log('Tab Title:', currentTab.title);
+      console.log('Tab URL:', currentTab.url);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <button onClick={getTabDetail} type="button">
+        Get current tab
+      </button>
+    </div>
   );
 }
 
